c627eda86757d605f69adc59aa46dd56df4ca49a
swastiksuman <swastiksuman@gmail.com>
2020-05-13 01:34:30 +0530

    simple procedure

--- ProcedureAndPackages.sql
+++ ProcedureAndPackages.sql
@@ -9,4 +9,15 @@ EXCEPTION
         DBMS_OUTPUT.PUT_LINE(SQLERRM);
         ROLLBACK;
         RAISE;
-END update_dept;        
+END update_dept;
+
+CREATE OR REPLACE PROCEDURE GIVE_HIKE AS
+
+BEGIN 
+  FOR cur_get_employees_var in (select emp_id from employee) LOOP
+    update employee set emp_sal = emp_sal * 1.1 where emp_id = cur_get_employees_var.emp_id;
+  END LOOP;
+  commit;
+END;  
+
+execute GIVE_HIKE
